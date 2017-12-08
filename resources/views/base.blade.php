a306b4960bc1c30c993883a7300ac58f9e2c1e59
IanBrison <buo1005@mail4.doshisha.ac.jp>
2017-12-08 14:46:38 +0900

    use textillate.js

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -2,11 +2,13 @@
 <html>
     <head>
         <title>@yield('title')</title>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>--}}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jcanvas/20.1.4/min/jcanvas.min.js"></script>
+        <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/lettering.js/0.7.0/jquery.lettering.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/textillate/0.4.0/jquery.textillate.min.js"></script>
         @yield('head')
     </head>
     <body>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,8 +1,94 @@
 @extends('base')
 
+@section('head')
+    <style>
+        h2 {
+            font-size: 20px;
+        }
+        #block {
+            height: 30px;
+        }
+        #explanation {
+            margin: 10px;
+            padding: 10px;
+            border: solid;
+            display: none;
+        }
+    </style>
+@endsection
+
 @section('body')
+    <div id="block"></div>
     <div class="container">
-        <p>実験のためのトップページです</p>
-        <a href="{{ action('ExperimentController@getStart') }}">実験開始</a>
+        <h2 class="tlt">
+            こんにちは、社会情報学研究室のブライソンです。今日は実験に参加してくれてどうもありがとう！
+        </h2>
+        <h2 class="tlt2">
+            これから３つのこと（オプションでもう一つも可能）をしてもらいます。想定している総時間は10分です。
+        </h2>
+        <div id="explanation" class="row">
+            <div class="col-xs-3">
+                <h2 class="">
+                    ①被験者情報の登録
+                </h2>
+            </div>
+            <div class="col-xs-9">
+                <h2 class="">
+                    学年・性別・学部・お名前などを入力してもらいます。実験にとって大切なのはもちろんのこと、④のためにも重要なのでよろしくお願いします。
+                </h2>
+            </div>
+            <div class="col-xs-3">
+                <h2 class="">
+                    ②実験
+                </h2>
+            </div>
+            <div class="col-xs-9">
+                <h2 class="">
+                    実際に実験を行ってもらいます。何をするかは後ほど教えます。途中でやめられるとデータが不完全になったりしてしまうので、始めたらなるべく最後まで終わらせてください。
+                </h2>
+            </div>
+            <div class="col-xs-3">
+                <h2 class="">
+                    ③アンケート
+                </h2>
+            </div>
+            <div class="col-xs-9">
+                <h2 class="">
+                    実験時に同時に逐次かんたんなアンケートを行います。実験とアンケートを5セット行ったら、実験は終了です。
+                </h2>
+            </div>
+            <div class="col-xs-3">
+                <h2 class="">
+                    ④図書カード500円分をもらう（オプション）
+                </h2>
+            </div>
+            <div class="col-xs-9">
+                <h2 class="">
+                    実験が終わりますと、受け取り場所を告知しますので好きな時に受け取りに来てください。
+                </h2>
+            </div>
+        </div>
     </div>
+
+    <div id="next" class="container text-right" style="display: none;">
+        <a href="{{ action('ExperimentController@getStart') }}">ちゃっちゃと実験に進みたい人はこちら</a>
+    </div>
+@endsection
+
+@section('script')
+    <script>
+        $('.tlt').textillate({autoStart: true, in : {effect: 'fadeInUp', sync: false, delayScale: 1.5}}).on('end.tlt', function() {
+            $('.tlt2').textillate('start');
+        });
+        $('.tlt2').textillate({autoStart: false, in : {effect: 'fadeInUp', sync: false, delayScale: 1.5}}).on('end.tlt', function() {
+            $('.tlt3').textillate('start');
+            $('#explanation').show(3000);
+        });
+        $('.tlt3').textillate({autoStart: false, in : {effect: 'fadeInUp', sync: true, delayScale: 1.5}}).on('end.tlt', function() {
+            $('.tlt4').textillate('start');
+        });
+        $('.tlt4').textillate({autoStart: false, in : {effect: 'fadeInUp', sync: true, delayScale: 1.5}}).on('end.tlt', function() {
+           $('#next').show();
+        });
+    </script>
 @endsection
